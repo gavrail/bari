f53b5746911878abee6c826ccb2cf9886cfcf13d
gavrail <gavrail@users.noreply.github.com>
2021-01-24 16:33:36 -0500

    Tests to replace targetGoals with targetGoalName and targetIncorporatedGoalsNames

--- systest/x86-x64-test/scripts/postprocessors/test.py
+++ systest/x86-x64-test/scripts/postprocessors/test.py
@@ -5,7 +5,7 @@ print 'Target dir:' + targetDir
 genExe = get_tool("fsrepo://MessageGenerator/*.*", "MessageGenerator.exe")
 
 cmd = genExe + ' "' + targetDir+os.path.sep+'generated.txt" "%s"' % ",".join(
-    ["Hello_world"] + list(targetGoals))
+    ["Hello_world", targetGoalName] + list(targetIncorporatedGoalsNames))
 
 if is_mono:
     cmd  = 'mono ' + cmd
